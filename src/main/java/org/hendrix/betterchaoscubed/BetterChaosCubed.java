0291ef4c48a92aed1debcab2086283cad6b43b90
JimiIT92 <14943331+JimiIT92@users.noreply.github.com>
2026-06-24 23:02:24 +0200

    Initial commit

---
+++ src/main/java/org/hendrix/betterchaoscubed/BetterChaosCubed.java
@@ -0,0 +1,18 @@
+package org.hendrix.betterchaoscubed;
+
+import net.fabricmc.api.ModInitializer;
+
+/**
+ * Better Chaos Cubed.
+ * Enhance Sulfur Cubes with redstone archetype, craftable nausea potions and more!
+ */
+public final class BetterChaosCubed implements ModInitializer {
+
+    /**
+     * Initialize the mod
+     */
+    @Override
+    public void onInitialize() {
+    }
+
+}
